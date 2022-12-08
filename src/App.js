e26729a1306dffe8b30741bdfd58693569e91447
yuchen_han+cs1300@brown.edu <yuchen_han+cs1300@brown.edu>
2022-12-08 15:55:34 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import ProjectItem from './components/ProjectItem.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <div className="header-title"><p>SprintingSloth</p></div>
+        <div className="header-menu">
+
+        </div>
+      </header>
+      <div className='body'>
+        <div className='intro'>
+          Hi I am bla bla
+        </div>
+        <div className='projects'>Projects
+        
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
