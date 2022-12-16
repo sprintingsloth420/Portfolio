eea9624aef421d554387071ada3a9a7a0d97cbb7
yuchen_han+cs1300@brown.edu <yuchen_han+cs1300@brown.edu>
2022-12-16 01:05:38 -0500

    try commit

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
       <App />
-    </HashRouter>
   </React.StrictMode>
 );
 
